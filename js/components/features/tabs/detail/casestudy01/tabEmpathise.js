0b6fd8864fd79d93f21d66e3a72e872e0cbc6629
ainabjerke <83291997+ainabjerke@users.noreply.github.com>
2023-10-31 14:43:42 +0100

    update
    update

--- js/components/features/tabs/detail/casestudy01/tabEmpathise.js
+++ js/components/features/tabs/detail/casestudy01/tabEmpathise.js
@@ -11,6 +11,28 @@
 //   document.getElementById(cityName).style.display = "block";
 //   evt.currentTarget.className += " active";
 // }
+// function openTabContent(evt, content) {
+//   var i, tabcontent, tablinks;
+//   tabcontent = document.getElementsByClassName("tabcontent");
+//   for (i = 0; i < tabcontent.length; i++) {
+//     tabcontent[i].style.display = "none";
+//   }
+//   tablinks = document.getElementsByClassName("tablinks");
+//   for (i = 0; i < tablinks.length; i++) {
+//     tablinks[i].className = tablinks[i].className.replace(" TabActive", "");
+//   }
+//   document.getElementById(content).style.display = "block";
+//   evt.currentTarget.className += " TabActive";
+// }
+
+document.addEventListener("DOMContentLoaded", function () {
+  // Trigger the first tab to be open when the page loads
+  openTabContent(
+    { currentTarget: document.querySelector(".tablinks") },
+    "tab1"
+  );
+});
+
 function openTabContent(evt, content) {
   var i, tabcontent, tablinks;
   tabcontent = document.getElementsByClassName("tabcontent");
@@ -19,8 +41,8 @@ function openTabContent(evt, content) {
   }
   tablinks = document.getElementsByClassName("tablinks");
   for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(" active", "");
+    tablinks[i].className = tablinks[i].className.replace(" TabActive", "");
   }
   document.getElementById(content).style.display = "block";
-  evt.currentTarget.className += " active";
+  evt.currentTarget.className += " TabActive";
 }
